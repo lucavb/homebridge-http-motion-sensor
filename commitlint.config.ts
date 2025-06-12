ea7623aefbdb822fc471f12055091fdaee6b4f8f
Luca Becker <luca.becker@me.com>
2025-06-12 09:44:40 +0200

    chore: refine commitlint configuration for improved message validation
    - Updated `commitlint.config.ts` to enhance rules for commit message validation.
    - Adjusted rules for subject case, subject empty checks, and type case enforcement.
    - Ensured compatibility with UserConfig type using TypeScript's `satisfies` operator.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,20 +1,20 @@
 import type { UserConfig } from '@commitlint/types';
 
-const config: UserConfig = {
+const config = {
     extends: ['@commitlint/config-conventional'],
     rules: {
+        'body-max-line-length': [0],
+        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
         'type-enum': [
             2,
             'always',
             ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
         ],
-        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-        'subject-full-stop': [2, 'never', '.'],
-        'subject-empty': [2, 'never'],
-        'type-empty': [2, 'never'],
-        'type-case': [2, 'always', 'lower-case'],
-        'body-max-line-length': [0],
     },
-};
+} as const satisfies UserConfig;
 
 export default config;
